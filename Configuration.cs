f1a64a7079fe4491a10b534cb486108e8181c588
sam bacha <32783916+mer1dian@users.noreply.github.com>
2019-10-25 18:13:35 -0700

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,11 @@
+using BlockarrayNetwork.Standard.Models;
+namespace BlockarrayNetwork.Standard
+{
+    public partial class Configuration
+    {
+
+        //The base Uri for API calls
+        public static string BaseUri = "http://example.com/api";
+
+    }
+}
